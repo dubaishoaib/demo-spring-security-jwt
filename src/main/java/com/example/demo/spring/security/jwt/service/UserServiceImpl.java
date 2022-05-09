39326c4381f7eb05a76664f3ab9c9c044a70140b
Shoaib Soomro <shoaib.soomro@gmail.com>
2022-05-10 00:13:29 +0400

    spring security jwt example

--- src/main/java/com/example/demo/spring/security/jwt/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/spring/security/jwt/service/UserServiceImpl.java
@@ -7,20 +7,45 @@ import com.example.demo.spring.security.jwt.repo.UserRepo;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Autowired
     private UserRepo userRepo;
     @Autowired
     private RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if(user == null){
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        }
+        log.info("User found in the database : {}", username);
+        Collection<? extends GrantedAuthority> authorities = user
+                .getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getPassword(), authorities);
+    }
 
     @Override
     public User saveUser(User user) {
@@ -53,4 +78,5 @@ public class UserServiceImpl implements UserService{
         log.info("Fetching all users");
         return userRepo.findAll();
     }
+
 }
